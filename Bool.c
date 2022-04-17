f9909b7bb9e9b4dec78c12234b361c03a4432cc7
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-04-17 22:36:01 +0530

    update

---
+++ Bool.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef enum { F, T } boolean;
+
+int main()
+{
+	boolean bool1, bool2;
+	bool1 = F;
+
+	if (bool1 == F) {
+		printf("bool1 is false\n");
+	}
+	else {
+		printf("bool1 is true\n");
+	}
+	bool2 = 2;
+	if (bool2 == F) {
+		printf("bool2 is false\n");
+	}
+	else {
+		printf("bool2 is true\n");
+	}
+}
