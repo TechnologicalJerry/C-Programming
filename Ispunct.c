e2f841ce973806c4452892f766d08f1c4b21b9d2
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-04-21 23:58:51 +0530

    update

---
+++ Ispunct.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+	int i;
+	printf("All punctuation characters in C"
+			" programming are: \n");
+	for (i = 0; i <= 255; ++i)
+		if (ispunct(i) != 0)
+			printf("%c ", i);
+	return 0;
+}
