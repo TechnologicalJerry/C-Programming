720372f9b35768d1a9fe2cba5fbba02c74bc2adb
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-04-29 23:24:55 +0530

    update

---
+++ BitFields.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct test1 {
+  unsigned int x : 5;
+  unsigned int y : 8;
+};
+
+struct test2 {
+  unsigned int x : 5;
+  unsigned int : 0;
+  unsigned int y : 8;
+};
+
+int main() {
+  printf("Size of test1 is %lu bytes\n", sizeof(struct test1));
+  printf("Size of test2 is %lu bytes\n", sizeof(struct test2));
+  return 0;
+}
