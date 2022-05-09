d211f561117a6200aa3fd8059cb074bd05c42589
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-05-09 23:31:32 +0530

    update

---
+++ Sequence.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int f1() {
+  printf("Geeks");
+  return 1;
+}
+int f2() {
+  printf("forGeeks");
+  return 1;
+}
+int main() {
+  int p = f1() + f2();
+  return 0;
+}
+
+#include <stdio.h>
+int x = 20;
+int f1() {
+  x = x + 10;
+  return x;
+}
+int f2() {
+  x = x - 5;
+  return x;
+}
+int main() {
+  int p = f1() + f2();
+  printf("p = %d", p);
+  return 0;
+}
+
+#include <stdio.h>
+int main() {
+  int i = 8;
+  int p = i++ * i++;
+  printf("%d\n", p);
+}
