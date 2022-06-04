ac3ec9843fb317660e520d8490b76a0ab623d4eb
technologicaljerry <cserajatverma@hotmail.com>
2022-06-04 22:58:23 +0530

    update

---
+++ Strlwr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char str[20];
+  printf("Enter string: ");
+  gets(str);
+  printf("String is: %s", str);
+  printf("\nLower String is: %s", strlwr(str));
+  return 0;
+}
