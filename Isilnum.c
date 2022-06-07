b27bc3f3dd8dc125c1d140ff34c1a10c0f4ea7c0
technologicaljerry <cserajatverma@hotmail.com>
2022-06-07 20:50:29 +0530

    update

---
+++ Isilnum.c
@@ -0,0 +1,49 @@
+#include <conio.h>
+#include <stdio.h>
+int main() {
+  // declaration of the variables
+  int ch1 = 'A';
+  int ch2 = 'e';
+  int ch3 = '$';
+  int ch4 = '7';
+  int ch5 = ' ';
+  int ch6 = '0';
+
+  if (isalnum(ch1)) {
+    printf(" '%c' is an alphanumeric character. \n", ch1);
+  } else {
+    printf("'%c' is not an alphabetic or numeric character. \n", ch1);
+  }
+
+  if (isalnum(ch2)) {
+    printf(" '%c' is an alphanumeric character. \n", ch2);
+  } else {
+    printf("'%c' is not an alphabetic or numeric character. \n", ch2);
+  }
+
+  if (isalnum(ch3)) {
+    printf(" '%c' is an alphanumeric character. \n", ch3);
+  } else {
+    printf("'%c' is not an alphabetic or numeric character. \n", ch3);
+  }
+
+  if (isalnum(ch4)) {
+    printf(" '%c' is an alphanumeric character. \n", ch4);
+  } else {
+    printf(" '%c' is not an alphabetic or numeric character. \n", ch4);
+  }
+
+  if (isalnum(ch5)) {
+    printf(" '%c' is an alphanumeric character. \n", ch5);
+  } else {
+    printf(" '%c' is not an alphabetic or numeric character. \n", ch5);
+  }
+
+  if (isalnum(ch6)) {
+    printf(" '%c' is an alphanumeric character. \n", ch6);
+  } else {
+    printf(" '%c' is not an alphabetic or numeric character. \n", ch6);
+  }
+
+  return 0;
+}
