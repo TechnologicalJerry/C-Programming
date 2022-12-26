e79a7ceb2fe9082402b78359b4959b9e03d30b0c
technologicaljerry <cserajatverma@gmail.com>
2022-12-26 23:31:19 +0530

    update structures

--- Structure.c
+++ Structure.c
@@ -1,19 +1,13 @@
-
-#include<stdio.h>
- 
-struct Point
-{
-   int x, y;
+#include <stdio.h>
+struct Point {
+  int x, y;
 };
- 
-int main()
-{
-   struct Point p1 = {1, 2};
- 
-   // p2 is a pointer to structure p1
-   struct Point *p2 = &p1;
- 
-   // Accessing structure members using structure pointer
-   printf("%d %d", p2->x, p2->y);
-   return 0;
+
+int main() {
+  struct Point p1 = {1, 2};
+
+  struct Point *p2 = &p1;
+
+  printf("%d %d", p2->x, p2->y);
+  return 0;
 }
